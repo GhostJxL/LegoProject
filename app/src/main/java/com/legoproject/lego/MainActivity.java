baea76da5141cafda17f7e52f5502ce1ed3d1e21
MandyG1015 <xiaor_1216@163.com>
2019-08-29 23:51:48 +0800

    修复了一些bug

--- app/src/main/java/com/legoproject/lego/MainActivity.java
+++ app/src/main/java/com/legoproject/lego/MainActivity.java
@@ -6,40 +6,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    private TextView tv01,tv02,tv03;
-    private ImageView iv01;
+    private TextView tv02,tv03;
+    private RelativeLayout tv01;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv01 = findViewById(R.id.main_camera);
+        tv01 = findViewById(R.id.main_cameralayout);
         tv02 = findViewById(R.id.main_photo);
         tv03 = findViewById(R.id.main_history);
-        iv01 = findViewById(R.id.main_logo);
 
         //添加监听器
         MyListener ml = new MyListener();
         tv01.setOnClickListener(ml);
         tv02.setOnClickListener(ml);
         tv03.setOnClickListener(ml);
-        iv01.setOnClickListener(ml);
 
     }
+
 
     class MyListener implements View.OnClickListener {
         public void onClick(View arg0) {
             switch (arg0.getId()) {
-                case (R.id.main_camera):
-                    scan();
-                    break;
-                case (R.id.main_logo):
+                case (R.id.main_cameralayout):
                     scan();
                     break;
                 case (R.id.main_photo):
