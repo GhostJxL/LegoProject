e9dfb89fff26aacba4bf69634e7675ac92d1ce50
MandyG1015 <xiaor_1216@163.com>
2019-08-29 21:28:39 +0800

    MainActivity beta it can only jump to ScanActivity by click "开始识别" main_layout 1.0 without bright colour

--- app/src/main/java/com/legoproject/lego/MainActivity.java
+++ app/src/main/java/com/legoproject/lego/MainActivity.java
@@ -5,12 +5,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    private TextView tv01,tv02;
+    private TextView tv01,tv02,tv03;
+    private ImageView iv01;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +21,15 @@ public class MainActivity extends Activity {
 
         tv01 = findViewById(R.id.main_camera);
         tv02 = findViewById(R.id.main_photo);
+        tv03 = findViewById(R.id.main_history);
+        iv01 = findViewById(R.id.main_logo);
 
         //添加监听器
         MyListener ml = new MyListener();
         tv01.setOnClickListener(ml);
         tv02.setOnClickListener(ml);
+        tv03.setOnClickListener(ml);
+        iv01.setOnClickListener(ml);
 
     }
 
@@ -31,11 +37,16 @@ public class MainActivity extends Activity {
         public void onClick(View arg0) {
             switch (arg0.getId()) {
                 case (R.id.main_camera):
-                    Toast.makeText(MainActivity.this, "successful", Toast.LENGTH_SHORT).show();
+                    scan();
+                    break;
+                case (R.id.main_logo):
                     scan();
                     break;
                 case (R.id.main_photo):
                     photobook();
+                    break;
+                case (R.id.main_history):
+                    showhistory();
                     break;
             }
         }
@@ -43,12 +54,18 @@ public class MainActivity extends Activity {
 
     public void scan(){
         Intent intent = new Intent(MainActivity.this, ScanActivity.class);
-        startActivity(intent);//跳转
-        finish();
+        startActivity(intent);//跳转至扫描界面
+//        finish();
     }
 
     public void photobook(){
 
+    }
+
+    public void showhistory(){
+        Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
+        startActivity(intent);//跳转至识别历史记录界面
+//        finish();
     }
 
 
