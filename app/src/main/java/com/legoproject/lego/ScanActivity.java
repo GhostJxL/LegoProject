ec5f09dfcc0a1ea4a6331c415b8fb7aa5eaaa399
MandyG1015 <xiaor_1216@163.com>
2019-08-29 21:57:44 +0800

    哭哭.txt

--- app/src/main/java/com/legoproject/lego/ScanActivity.java
+++ app/src/main/java/com/legoproject/lego/ScanActivity.java
@@ -1,34 +1,105 @@
 package com.legoproject.lego;
 
+import android.Manifest;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
 
-public class ScanActivity extends AppCompatActivity {
+public class ScanActivity extends AppCompatActivity implements CvCameraViewListener2 {
+
+    private JavaCameraView myCamera;
+    private boolean a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_scan);
-        boolean success = OpenCVLoader.initDebug();
-        Bitmap bitmap = BitmapFactory.decodeResource(
-                this.getResources(), R.drawable.test
-        );
-        Mat src = new Mat();
-        Mat dst = new Mat();
-        Utils.bitmapToMat(bitmap,src);
-        Imgproc.cvtColor(src,dst,Imgproc.COLOR_BGRA2GRAY);
-        Utils.matToBitmap(dst,bitmap);
-        ImageView iv = this.findViewById(R.id.scan_img);
-        iv.setImageBitmap(bitmap);
-        src.release();
-        dst.release();
+        if (!OpenCVLoader.initDebug()) {
+            // Handle initialization error
+        }
+
+        //相机动态授权
+        if (Build.VERSION.SDK_INT >= 23) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.CAMERA,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    1);
+        }
+
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        myCamera = findViewById(R.id.main_camera);
+        myCamera.setVisibility(SurfaceView.VISIBLE);
+        myCamera.setCvCameraViewListener(this);
+        myCamera.setCameraIndex(0); //使用后置摄像头
+        if (myCamera != null) {
+            myCamera.disableView();
+        }
+        myCamera.enableView();
+
+
+        //----向日葵测试
+//        boolean success = OpenCVLoader.initDebug();
+//        Bitmap bitmap = BitmapFactory.decodeResource(
+//                this.getResources(), R.drawable.test
+//        );
+//        Mat src = new Mat();
+//        Mat dst = new Mat();
+//        Utils.bitmapToMat(bitmap, src);
+//        Imgproc.cvtColor(src, dst, Imgproc.COLOR_BGRA2GRAY);
+//        Utils.matToBitmap(dst, bitmap);
+//        ImageView iv = this.findViewById(R.id.scan_img);
+//        iv.setImageBitmap(bitmap);
+//        src.release();
+//        dst.release();
+        //--------------------------------------
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+
+        Mat frame = inputFrame.rgba();
+        if (this.getResources().getConfiguration().orientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT) {
+            Core.rotate(frame, frame, Core.ROTATE_90_CLOCKWISE);
+        }
+        return frame;
+    }
+
 }
